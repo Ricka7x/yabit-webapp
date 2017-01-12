c39d2a781eb2bd26b101b1ba9c121668adfbe6c5
ricka7x <ricky.ramirez89@gmail.com>
2017-01-12 14:00:15 -0600

    Define project structure

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+// styles
+import styles from './app.scss'
+
+class App extends Component {
+  render () {
+    return (
+      <div className={styles.App}>Hello React</div>
+    )
+  }
+}
+
+export default App
